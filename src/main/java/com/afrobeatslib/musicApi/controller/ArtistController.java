93fad9c648d20b1d680f37cdaa8742f10db54a1e
ofemiashiru <ofemiashiru@gmail.com>
2024-12-03 21:43:50 +0000

    add controller layer for both artist and genre

---
+++ src/main/java/com/afrobeatslib/musicApi/controller/ArtistController.java
@@ -0,0 +1,25 @@
+package com.afrobeatslib.musicApi.controller;
+
+import com.afrobeatslib.musicApi.dto.ArtistDto;
+import com.afrobeatslib.musicApi.service.ArtistService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+@Controller
+public class ArtistController {
+
+    private ArtistService artistService;
+
+    @Autowired
+    public ArtistController(ArtistService artistService){
+        this.artistService = artistService;
+    }
+
+    @QueryMapping
+    public List<ArtistDto> getArtists(){
+        return artistService.getArtists();
+    }
+}
---
+++ src/main/java/com/afrobeatslib/musicApi/controller/GenreController.java
@@ -0,0 +1,25 @@
+package com.afrobeatslib.musicApi.controller;
+
+import com.afrobeatslib.musicApi.model.Genre;
+import com.afrobeatslib.musicApi.service.GenreService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+@Controller
+public class GenreController {
+
+    GenreService genreService;
+
+    @Autowired
+    public GenreController(GenreService genreService){
+        this.genreService = genreService;
+    }
+
+    @QueryMapping
+    List<Genre> getGenres(){
+        return genreService.getGenres();
+    }
+}
