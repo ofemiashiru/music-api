243c581ebeafe6ac1e100e5448878c3d2cdec47e
ofemiashiru <ofemiashiru@gmail.com>
2024-12-03 21:46:28 +0000

    add function to populate database with artists

---
+++ src/main/java/com/afrobeatslib/musicApi/ArtistConfig.java
@@ -0,0 +1,38 @@
+package com.afrobeatslib.musicApi;
+
+import com.afrobeatslib.musicApi.model.Artist;
+import com.afrobeatslib.musicApi.repository.ArtistRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+import java.util.UUID;
+
+@Configuration
+public class ArtistConfig {
+    @Bean
+    CommandLineRunner artistCommandLineRunner(ArtistRepository artistRepository){
+        return args -> {
+            Artist felaKuti = new Artist(
+                    UUID.randomUUID(),
+                    "Fela Kuti",
+                    1
+            );
+            Artist burnaBoy = new Artist(
+                    UUID.randomUUID(),
+                    "Burna Boy",
+                    2
+            );
+            Artist ayraStarr = new Artist(
+                    UUID.randomUUID(),
+                    "Ayra Starr",
+                    4
+            );
+
+            artistRepository.saveAll(
+                    List.of(felaKuti, burnaBoy, ayraStarr)
+            );
+        };
+    }
+}
