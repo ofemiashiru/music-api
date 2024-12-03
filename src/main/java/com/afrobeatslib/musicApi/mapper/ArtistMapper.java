519cf19fb7814693ae85f2ff0540428f5e1f2e42
ofemiashiru <ofemiashiru@gmail.com>
2024-12-03 21:42:34 +0000

    create mapper to transform dto to entity and vice versa

---
+++ src/main/java/com/afrobeatslib/musicApi/mapper/ArtistMapper.java
@@ -0,0 +1,41 @@
+package com.afrobeatslib.musicApi.mapper;
+
+import com.afrobeatslib.musicApi.dto.ArtistDto;
+import com.afrobeatslib.musicApi.dto.ArtistInputDto;
+import com.afrobeatslib.musicApi.model.Artist;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class ArtistMapper {
+
+    public ArtistDto toDto(Artist artist){
+        if(artist == null){
+            return null;
+        }
+
+        ArtistDto dto = new ArtistDto();
+
+        dto.setId(artist.getId());
+        dto.setArtistName(artist.getArtistName());
+        dto.setArtistGenreId(artist.getArtistGenreId());
+
+        return dto;
+    }
+
+    public Artist toEntity(ArtistInputDto dto){
+
+        if(dto == null){
+            return null;
+        }
+
+        Artist entity = new Artist();
+
+        entity.setId(UUID.fromString(dto.getId()));
+        entity.setArtistName(dto.getArtistName());
+        entity.setArtistGenreId(dto.getArtistGenreId());
+
+        return entity;
+    }
+}
