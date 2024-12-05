2a2091b519423289a30c362a0f546ad3fbb3d18b
ofemiashiru <ofemiashiru@gmail.com>
2024-12-05 13:04:10 +0000

    add main entry point for spring app and corresponding test

---
+++ src/test/java/com/afrobeatslib/musicApi/MusicApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.afrobeatslib.musicApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
