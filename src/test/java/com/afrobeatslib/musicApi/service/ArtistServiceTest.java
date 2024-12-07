3d22a3f053cbedc6feadd6a53c55713de3abef61
ofemiashiru <ofemiashiru@gmail.com>
2024-12-07 17:58:01 +0000

    remove unused imports

--- src/test/java/com/afrobeatslib/musicApi/service/ArtistServiceTest.java
+++ src/test/java/com/afrobeatslib/musicApi/service/ArtistServiceTest.java
@@ -10,9 +10,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
