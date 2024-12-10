090149466fde943cf852d4c732a9fc1c6597fef8
ofemiashiru <ofemiashiru@gmail.com>
2024-12-10 23:59:31 +0000

    remove unused import

--- src/test/java/com/afrobeatslib/musicApi/service/ArtistServiceTest.java
+++ src/test/java/com/afrobeatslib/musicApi/service/ArtistServiceTest.java
@@ -16,7 +16,6 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
