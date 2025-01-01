2fbc53018350c80dd151055897fdc3cd67b3707c
ofemiashiru <ofemiashiru@gmail.com>
2025-01-01 23:37:43 +0000

    re organise

--- src/main/java/com/afrobeatslib/musicApi/config/InitialConfig.java
+++ src/main/java/com/afrobeatslib/musicApi/config/InitialConfig.java
@@ -14,83 +14,69 @@ import java.util.UUID;
 
 @Configuration
 public class InitialConfig {
-    @Bean
-    CommandLineRunner artistCommandLineRunner(ArtistRepository artistRepository, GenreRepository genreRepository){
-  
-        return args -> {
-            Genre afrobeat = new Genre(
-                1,
-                "Afrobeat"
-            );
-            Genre afrobeats = new Genre(
-                2,
-                "Afrobeats"
-            );
-            Genre afrofusion = new Genre(
-                3,
-                "Afrofusion"
-            );
-            Genre afropop = new Genre(
-                4,
-                "Afropop"
-            );
-            Genre amapiano = new Genre(
-                5,
-                "Amapiano"
-            );
-            Genre banku = new Genre(
-                6,
-                "Banku"
-            );
-            Genre fuji = new Genre(
-                7,
-                "Fuji"
-            );
-            Genre gospel = new Genre(
-                8,
-                "Gospel"
-            );
-            Genre highlife = new Genre(
-                9,
-                "Highlife"
-            );
-            Genre hiphop = new Genre(
-                10,
-                "Hip-hop"
-            );
-            genreRepository.saveAll(
-                List.of(afrobeat, afrobeats, afrofusion,
-                        afropop, amapiano, banku, fuji,
-                        gospel, highlife, hiphop
-                )
-            );
+        @Bean
+        CommandLineRunner artistCommandLineRunner(ArtistRepository artistRepository, GenreRepository genreRepository) {
 
-            Artist felaKuti = new Artist(
-                UUID.randomUUID(),
-                "Fela Kuti",
-                "https://demopractice.blob.core.windows.net/images/placeholder-user-profile.jpg"
-            );
-            felaKuti.addGenre(afrobeat);
+                return args -> {
+                        Genre afrobeat = new Genre(
+                                        1,
+                                        "Afrobeat");
+                        Genre afrobeats = new Genre(
+                                        2,
+                                        "Afrobeats");
+                        Genre afrofusion = new Genre(
+                                        3,
+                                        "Afrofusion");
+                        Genre afropop = new Genre(
+                                        4,
+                                        "Afropop");
+                        Genre amapiano = new Genre(
+                                        5,
+                                        "Amapiano");
+                        Genre banku = new Genre(
+                                        6,
+                                        "Banku");
+                        Genre fuji = new Genre(
+                                        7,
+                                        "Fuji");
+                        Genre gospel = new Genre(
+                                        8,
+                                        "Gospel");
+                        Genre highlife = new Genre(
+                                        9,
+                                        "Highlife");
+                        Genre hiphop = new Genre(
+                                        10,
+                                        "Hip-hop");
+                        genreRepository.saveAll(
+                                        List.of(
+                                                        afrobeat, afrobeats, afrofusion,
+                                                        afropop, amapiano, banku, fuji,
+                                                        gospel, highlife, hiphop));
 
-            Artist burnaBoy = new Artist(
-                UUID.randomUUID(),
-                "Burna Boy",
-                "https://demopractice.blob.core.windows.net/images/placeholder-user-profile.jpg"
-            );
-            burnaBoy.addGenre(afrobeats);
-            burnaBoy.addGenre(afrofusion);
+                        Artist felaKuti = new Artist(
+                                        UUID.randomUUID(),
+                                        "Fela Kuti",
+                                        "https://musicfileuploads.blob.core.windows.net/images/fela-kuti.webp");
 
-            Artist ayraStarr = new Artist(
-                UUID.randomUUID(),
-                "Ayra Starr",
-                "https://demopractice.blob.core.windows.net/images/placeholder-user-profile.jpg"
-            );
-            ayraStarr.addGenre(afropop);
-            ayraStarr.addGenre(afrobeats);
+                        Artist burnaBoy = new Artist(
+                                        UUID.randomUUID(),
+                                        "Burna Boy",
+                                        "https://musicfileuploads.blob.core.windows.net/images/burna-boy.webp");
 
-            artistRepository.saveAll(
-                List.of(felaKuti, burnaBoy, ayraStarr)
-            );
-        };
-    }
+                        Artist ayraStarr = new Artist(
+                                        UUID.randomUUID(),
+                                        "Ayra Starr",
+                                        "https://musicfileuploads.blob.core.windows.net/images/ayra-starr.webp");
+
+                        felaKuti.addGenre(afrobeat);
+                        burnaBoy.addGenre(afrobeats);
+                        burnaBoy.addGenre(afrofusion);
+                        ayraStarr.addGenre(afropop);
+                        ayraStarr.addGenre(afrobeats);
+
+                        artistRepository.saveAll(
+                                        List.of(felaKuti, burnaBoy, ayraStarr));
+                };
+        }
 }
